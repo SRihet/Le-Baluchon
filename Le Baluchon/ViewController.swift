3521eac4556cb8a3e7aa6925d76746ca89bdcc69
Stéphane Rihet <stephane.rihet@gmail.com>
2022-02-02 11:34:39 +0100

    Initial Commit

---
+++ Le Baluchon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Le Baluchon
+//
+//  Created by Stéphane Rihet on 02/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
